120a4670c44ab7084c9ea898c4c181d9b3020223
Rishi Desai <rishindesai@yahoo.com>
2025-01-09 06:00:39 -0500

    track list

--- screens/Search.tsx
+++ screens/Search.tsx
@@ -1,23 +1,23 @@
 import { useState, useCallback, useRef } from 'react';
-import { Text, FlatList, TouchableOpacity, Image, ActivityIndicator, StyleSheet } from 'react-native';
+import { FlatList, ActivityIndicator, StyleSheet } from 'react-native';
 import axios from 'axios';
 import { Box } from "@/gluestack/ui/box";
 
-import { MUSIC_SERVICE_BASE_URL } from '@/constants';
-
 import { throttle } from 'lodash';
+
+import { MUSIC_SERVICE_BASE_URL, TRACK_COUNT_LIMIT_PER_CALL, THROTTLE_DELAY_MILLISECONDS } from '@/constants';
+
 import SearchBar from '@/components/SearchBar';
+import TrackTile from '@/components/TrackTile';
 
 const SearchScreen = ({ navigation }: any) => {
-  const [searchQuery, setSearchQuery] = useState("");
   const [tracks, setTracks] = useState<Track[]>([]);
   const [loading, setLoading] = useState(false);
-  const limit = useRef(20);
+  const limit = useRef(TRACK_COUNT_LIMIT_PER_CALL);
   const totalQueryResults = useRef(0);
 
   const fetchData = async (query: string, newQuery: boolean) => {
     try {
-      console.log(query);
       let trimmedQuery = query.trim()
       if (!trimmedQuery) {
         setTracks([]);
@@ -29,7 +29,6 @@ const SearchScreen = ({ navigation }: any) => {
         index = 0;
       }
       const response = await axios.get(`${MUSIC_SERVICE_BASE_URL}/search?q=${trimmedQuery}&index=${index}&limit=${limit.current}`);
-      // console.log(response);
       totalQueryResults.current = response.data.total;
       if (newQuery) {
         setTracks(response.data.data);
@@ -46,28 +45,33 @@ const SearchScreen = ({ navigation }: any) => {
   const handleSearch = useCallback(
     throttle((query: string) => {
       fetchData(query, true);
-    }, 1000),
+    }, THROTTLE_DELAY_MILLISECONDS),
     []
   );
 
-  const handleEndReached = () => {
-    if (totalQueryResults.current < tracks.length) {
-      fetchData(searchQuery, false);
-    }
-  }
-
   return (
-    <Box flex={1} p={4} style={{ backgroundColor: "#121212" }}>
+    <Box style={styles.container}>
 
       <SearchBar handleSearch={handleSearch} />
 
-      
+      <FlatList
+        data={tracks}
+        renderItem={({ item: track } : { item: Track }) => (
+          <TrackTile navigation={navigation} track={track} />
+        )}
+        keyExtractor={(item) => item.id.toString()}
+        ListFooterComponent={loading ? <ActivityIndicator size="small" /> : null}
+      />
     </Box>
   );
 };
 
 const styles = StyleSheet.create({
-
+  container: {
+    backgroundColor: "#121212",
+    flex: 1,
+    padding: 4
+  }
 });
 
 export default SearchScreen;
