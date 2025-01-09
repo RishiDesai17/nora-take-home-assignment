78acdf621eeddc4d6a88f269a83ef4e9b29478a9
Rishi Desai <rishindesai@yahoo.com>
2025-01-09 16:49:51 -0500

    handled styles in stylesheet

--- components/TrackTile.tsx
+++ components/TrackTile.tsx
@@ -8,10 +8,10 @@ type TrackTileProps = {
 
 const TrackTile = ({ navigation, track }: TrackTileProps) => {
   return (
-    <TouchableOpacity onPress={() => navigation.navigate('TrackDetails', { track })} activeOpacity={0.7} style={{ margin: 5 }}>
+    <TouchableOpacity onPress={() => navigation.navigate('TrackDetails', { track })} activeOpacity={0.7} style={styles.clickableContainer}>
       <Box style={styles.container}>
         <Image source={{ uri: track.album.cover }} style={styles.image} />
-        <Box style={{ marginLeft: 20 }}>
+        <Box style={styles.infoContainer}>
           <Text numberOfLines={1} ellipsizeMode="tail" style={styles.title}>{track.title}</Text>
           <Text style={styles.artistName}>{track.artist.name}</Text>
         </Box>
@@ -28,6 +28,12 @@ const styles = StyleSheet.create({
     padding: 4,
     borderRadius: 8,
     width: "80%"
+  },
+  clickableContainer: {
+    margin: 5
+  },
+  infoContainer: {
+    marginLeft: 30
   },
   title: {
     fontWeight: "bold",
