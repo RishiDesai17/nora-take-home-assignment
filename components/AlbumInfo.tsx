9ab326049ab059aca081a016a9b6ab4d4f45cbd3
Rishi Desai <rishindesai@yahoo.com>
2025-01-09 06:22:55 -0500

    album info component

---
+++ components/AlbumInfo.tsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import { View, Text, Image, StyleSheet } from 'react-native';
+
+type Props = {
+  album: Album;
+};
+
+const AlbumInfo = ({ album }: Props) => {
+  return (
+    <View style={styles.albumContainer}>
+      <Text style={styles.albumTitle}>Album</Text>
+      <Image source={{ uri: album.cover_medium }} style={styles.albumImage} />
+      <Text style={styles.albumName}>{album.title}</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  albumContainer: {
+    alignItems: 'center',
+    marginTop: 15,
+    marginBottom: 50,
+  },
+  albumTitle: {
+    fontSize: 32,
+    fontWeight: "bold",
+    marginBottom: 20,
+    textAlign: "left",
+    color: 'white'
+  },
+  albumName: {
+    fontSize: 22,
+    color: '#B3B3B3',
+    marginBottom: 20,
+    textAlign: 'center',
+  },
+  albumImage: {
+    width: 160,
+    height: 160,
+    borderRadius: 15,
+    marginBottom: 20,
+  },
+});
+
+export default AlbumInfo;
